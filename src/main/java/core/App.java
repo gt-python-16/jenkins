d804a5e6a6441858e5616be4ebce63b93ceed67b
gt-java <galinaTim0417@gmail.com>
2017-05-12 14:36:16 -0700

    changes commit Anagram

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -2,8 +2,9 @@ package core;
  
 public class App {
        public static void main(String[] args) {
-    //System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
+    System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
     System.out.println("ab and ba is anagram? - " + Anagram.validate("ab", "ba"));
+    //System.out.println("ab and ba is anagram? - " + Anagram.validate("ab", "ba"));
        }
 }
 
